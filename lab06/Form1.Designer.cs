f56b0b4a2a460cbea63db995359e7603523bd3a9
vovahalaman <khalamanvova@gmail.com>
2023-03-24 11:13:03 +0200

    Додано калькулятор

--- lab06/Form1.Designer.cs
+++ lab06/Form1.Designer.cs
@@ -28,13 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.firstNumberTextBox = new System.Windows.Forms.TextBox();
+            this.secondNumberTextBox = new System.Windows.Forms.TextBox();
+            this.sumNumberTextBox = new System.Windows.Forms.TextBox();
+            this.plusButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // firstNumberTextBox
+            // 
+            this.firstNumberTextBox.Location = new System.Drawing.Point(491, 151);
+            this.firstNumberTextBox.Name = "firstNumberTextBox";
+            this.firstNumberTextBox.Size = new System.Drawing.Size(352, 30);
+            this.firstNumberTextBox.TabIndex = 0;
+            // 
+            // secondNumberTextBox
+            // 
+            this.secondNumberTextBox.Location = new System.Drawing.Point(491, 201);
+            this.secondNumberTextBox.Name = "secondNumberTextBox";
+            this.secondNumberTextBox.Size = new System.Drawing.Size(352, 30);
+            this.secondNumberTextBox.TabIndex = 3;
+            // 
+            // sumNumberTextBox
+            // 
+            this.sumNumberTextBox.Location = new System.Drawing.Point(491, 308);
+            this.sumNumberTextBox.Name = "sumNumberTextBox";
+            this.sumNumberTextBox.Size = new System.Drawing.Size(352, 30);
+            this.sumNumberTextBox.TabIndex = 4;
+            // 
+            // plusButton
+            // 
+            this.plusButton.Location = new System.Drawing.Point(617, 253);
+            this.plusButton.Name = "plusButton";
+            this.plusButton.Size = new System.Drawing.Size(103, 31);
+            this.plusButton.TabIndex = 5;
+            this.plusButton.Text = "+";
+            this.plusButton.UseVisualStyleBackColor = true;
+            this.plusButton.Click += new System.EventHandler(this.plusButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(491, 371);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(107, 35);
+            this.clearButton.TabIndex = 6;
+            this.clearButton.Text = "Очистити";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(736, 371);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(107, 35);
+            this.exitButton.TabIndex = 7;
+            this.exitButton.Text = "Вихід";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1333, 762);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.plusButton);
+            this.Controls.Add(this.sumNumberTextBox);
+            this.Controls.Add(this.secondNumberTextBox);
+            this.Controls.Add(this.firstNumberTextBox);
+            this.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "Lab06: Довга сума";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox firstNumberTextBox;
+        private System.Windows.Forms.TextBox secondNumberTextBox;
+        private System.Windows.Forms.TextBox sumNumberTextBox;
+        private System.Windows.Forms.Button plusButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
     }
 }
 
--- lab06/Form1.cs
+++ lab06/Form1.cs
@@ -16,5 +16,75 @@ namespace lab06
         {
             InitializeComponent();
         }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            foreach (TextBox textBox in Controls.OfType<TextBox>())
+                textBox.Text = "";
+        }
+
+        private void plusButton_Click(object sender, EventArgs e)
+        {
+            int maxLen = 1 +
+                Math.Max(firstNumberTextBox.Text.Length, secondNumberTextBox.TextLength);
+
+            int[] number1 = new int[maxLen];
+            int[] number2 = new int[maxLen];
+            int[] sum = new int[maxLen];
+            int[] extra = new int[maxLen];
+
+            int numberIdx, textIdx;
+
+            numberIdx = maxLen - 1;
+            textIdx = firstNumberTextBox.Text.Length - 1;
+
+            while (textIdx >= 0)
+            {
+                number1[numberIdx] = (firstNumberTextBox.Text[textIdx] - '0');
+                textIdx--;
+                numberIdx--;
+            }
+
+            numberIdx = maxLen - 1;
+            textIdx = secondNumberTextBox.Text.Length - 1;
+
+            while (textIdx >= 0)
+            {
+                number2[numberIdx] = (secondNumberTextBox.Text[textIdx] - '0');
+                textIdx--;
+                numberIdx--;
+            }
+
+            for (numberIdx = maxLen - 1; numberIdx > 0; numberIdx--)
+            {
+                sum[numberIdx] = (number1[numberIdx] + number2[numberIdx] + extra[numberIdx]) % 10;
+
+                extra[numberIdx - 1] = (number1[numberIdx] + number2[numberIdx] + extra[numberIdx]) / 10;
+            }
+            sum[0] = extra[0];
+
+            bool isNumber = false;
+            foreach (int digit in sum)
+            {
+                if (digit > 0)
+                {
+                    isNumber = true;
+                    sumNumberTextBox.Text += digit;
+                }
+
+                else
+                {
+                    if (isNumber)
+                    {
+                        sumNumberTextBox.Text += digit;
+                    }
+                }
+            }
+        }
     }
 }
